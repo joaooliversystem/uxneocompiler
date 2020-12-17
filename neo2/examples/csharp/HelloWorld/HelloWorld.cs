14e1b05703627e672aa8729fd227e88203944802
Vitor <vncoelho@gmail.com>
2020-12-16 21:41:19 -0300

    Adding resources for neo2 and link to legacy

---
+++ neo2/examples/csharp/HelloWorld/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using Neo.SmartContract.Framework.Services.Neo;
+
+namespace Neo.SmartContract
+{
+    public class HelloWorld : Framework.SmartContract
+    {
+        public static void Main()
+        {
+            // writes value "World" on storage key "Hello"
+            // implicitly calls Storage.Put(Storage.CurrentContext, "Hello", "World")
+            Storage.Put("Hello", "World");
+        }
+    }
+}
